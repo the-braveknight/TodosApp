0a1a25524dcb8b4454018a07d1c6b8e17e6f2a17
the-braveknight <zaid.riadh@gmail.com>
2024-12-29 15:52:58 +0300

    Initial Commit

---
+++ TodosApp/TodosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosAppApp.swift
+//  TodosApp
+//
+//  Created by Zaid Rahhawi on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TodosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
